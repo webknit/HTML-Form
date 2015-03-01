b751217ad53563b9bf718a5269ef007abc00aac8
Shane Prendergast <contact@webknit.co.uk>
2015-03-01 19:54:45 +0000

    atr at point where might stuggle to use OOP

--- script.js
+++ script.js
@@ -1,44 +1,75 @@
-/* - BASE HTML TEMPLATE
-------------------------------------------------- 
-	Description: JS Scripts
-	Author:Shane Prendergast
-	Author URL:http://www.webknit.co.uk
-	Template URL:http://base.webknit.co.uk/
-*/
+var Form = Form || {};
 
-// JS EXAMPLE
+Form.Submitted = false;
 
-var Base = Base || {};
+Form.ErrorText = {
+	input: 'This field is required',
+	email: 'This email is incorrect'	
+}
 
-Base.functionName = function()
-{	
-	var self = $(this);
-	var variable = $('.var');
+Form.Elements = {
+	errorMessage: $('.form__error'),
+	inputCheck: $('.form__input-check'),
+	emailCheck: $('.form__email-check'),
+	passwordCheck: $('.form__password-check'),
+}
 
-	function init()
-	{
-		variable.click(functionOne);
-		functionTwo();
+Form.checkForm = function() {
+
+	var formOk = true;
+
+	Form.Elements.errorMessage.removeClass('form__error--show');
+
+	Form.Elements.inputCheck.each(function() {
+	
+		if($(this).val() === '') {
+		
+			$(this).next('.form__error').addClass('form__error--show');
+			
+			formOk == false;
+		
+		}
+		
+	});
+	
+	Form.Elements.emailCheck.each(function() {
+	
+		var email = $(this).val();
+		
+		var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+		
+		if (email == '' || !re.test(email)) {
+		
+		    $(this).next('.form__error').addClass('form__error--show');
+		    
+		    formOk == false;
+		    
+		}
+		
+	});
+	
+	var pass1 = $('#password1').val();
+	var pass2 = $('#password2').val();
+	
+	console.log('pass1 ==' + pass1);
+	
+	if(pass1 !== pass1 || pass1 == '' || pass2 == '') {
+	
+		$('#password2').next('.form__error').addClass('form__error--show');
+		
+		formOk == false;
+	
 	}
-
-	function functionOne()
-	{
-		// JS CODE
-	}
-
-	function functionTwo()
-	{
-		// JS CODE
-		window.hide();
-	}
-
-	init();
+	
+	if(!formOk) return false;
+	
 };
 
-// ON DOC READY
-$(function()
-{	
-	new Base.functionName();
-	
-});
+$('.form').submit(function(e) {
 
+	e.preventDefault();
+
+	Form.checkForm();
+
+    
+}); 
